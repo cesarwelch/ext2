047946f83132abe35be0c0b7340c0fc4a988f916
José Mario López <jmlb@Joses-MacBook-Pro.local>
2015-11-15 21:41:27 -0600

    ya popula el inode bitmap

--- ext2/src/ext2/InodeBitmapControl.java
+++ ext2/src/ext2/InodeBitmapControl.java
@@ -5,10 +5,27 @@
  */
 package ext2;
 
+import java.util.BitSet;
+
 /**
  *
  * @author jmlb
  */
 public class InodeBitmapControl {
+    
+    fileSystemManager fsm = new fileSystemManager();
+    String info = fsm.getInodeBitmap();
+    int bitSize = info.length();
+    BitSet ibc = new BitSet(bitSize);
+    
+    
+    public void fillBitSet(){
+        for (int i = 0; i < info.length(); i++) {
+            if(Integer.parseInt(Character.toString(info.charAt(i))) == 1){
+                ibc.set(i, true);
+            }
+            
+        }
+    }
     
 }
